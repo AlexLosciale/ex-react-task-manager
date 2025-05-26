37261ccb79505f310c87f81ffe6fd9c89851499c
AlexLosciale <losciale17@gmail.com>
2025-05-26 15:22:18 +0200

    add: serch

--- react-task-manager-front/src/pages/TaskList.jsx
+++ react-task-manager-front/src/pages/TaskList.jsx
@@ -4,24 +4,40 @@ import TaskRow from '../components/TaskRow';
 export default function TaskList() {
   const { tasks } = useGlobalContext();
   
-  return (
-    <div className="container mt-5">
-      <h1 className="mb-4">Lista dei Task</h1>
-      <table className="table table-striped table-hover">
-        <thead className="table-dark">
-          <tr>
-            <th scope="col">Nome</th>
-            <th scope="col">Descrizione</th>
-            <th scope="col">Stato</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <TaskRow key={task.id} task={task} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+      <div className="container mt-5">
+        <div className=" p-4">
+          <h1 className="mb-4">Cerca Task</h1>
+    
+          <div className="mb-4">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Cerca per nome..."
+            />
+          </div>
+    
+          <h2 className="mb-3">Lista dei Task</h2>
+    
+          <div className="table-responsive">
+            <table className="table table-striped table-hover align-middle">
+              <thead className="table-dark">
+                <tr>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Descrizione</th>
+                  <th scope="col">Stato</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasks.map((task) => (
+                  <TaskRow key={task.id} task={task} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
 }
+    
 
